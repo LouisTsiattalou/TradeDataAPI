bfeb47d3a378f1ab964da52fb0dc355b7a131a72
Louis Tsiattalou <louis.tsi@gmail.com>
2020-02-18 19:06:35 +0000

    Add Data Download to Monthly Update script

---
+++ tradedata/update/monthly_update.py
@@ -0,0 +1,111 @@
+"""
+TITLE: Monthly Update
+AUTHOR: Louis Tsiattalou
+DATE STARTED: 2020-02-17
+REPOSITORY: https://github.com/LouisTsiattalou/TradeDataAPI
+DESCRIPTION:
+Update the database with each month's new data.
+"""
+
+import wget
+from pathlib import Path
+
+from tradedata.initialise.download_data import unzip_trade_data
+from tradedata.initialise.create_database import connect_to_postgres
+from tradedata.initialise.create_database import etl_control_table
+from tradedata.initialise.create_database import etl_trade_table
+
+def download_individual_zipfiles(dest_path, prefixes, month = "01", year = "20"):
+    """Downloads single month trade data zip files from UKTradeInfo"""
+    url = "https://www.uktradeinfo.com/Statistics/Documents/Data Downloads/"
+    dest_path = str(dest_path.absolute())
+    os.makedirs(dest_path, exist_ok=True)
+
+    def trade_data_download(remote_file_path, destination = dest_path):
+        """Download data if it can be found. Return remote file name if exception occurs"""
+        try:
+            wget.download(remote_file_path, out = destination)
+            return None
+        except:
+            return remote_file_path
+
+    # Loop over Trade Data files from prefixes
+    fails = []
+    for prefix in prefixes:
+        fails.append(trade_data_download(f"{url}{prefix}{year}{month}.zip", dest_path))
+
+    # Print failed downloads
+    fails = [x for x in fails if x is not None]
+    if len(fails) == 0:
+        print("All Zip Files Downloaded.")
+    else:
+        print("The following Zip Files could not be downloaded:")
+        print("\n".join(fails))
+
+
+if __name__ == '__main__':
+
+    # Parse Arguments
+    parser = argparse.ArgumentParser(description="Load a recent month's trade data to the database.")
+    parser.add_argument("-t", "--target",
+                        help="Target directory for the files. Created if it does not exist.",
+                        required = True, default = 'data/monthlyupdate/')
+    parser.add_argument("--year",
+                        help="Year to be downloaded; 2 digit string.",
+                        required = True, default = '2020')
+    parser.add_argument("--month",
+                        help="Month to be downloaded; 2 digit string.",
+                        required = True, default = '01')
+
+    # Params
+    args = parser.parse_args()
+    data_dir = args.target
+    data_year= args.year
+    data_month = args.month
+
+    # Handle %Y format (YYYY)
+    if len(data_year) == 4:
+        data_year = data_year[2:]
+
+    # DATA REFERENCES ====================================================================
+    # Table - Table Specification JSON filepath mapping
+    table_specs = {
+        "control":"data/lookups/controlfilecols.json",
+        "exports":"data/lookups/noneuexportcols.json",
+        "imports":"data/lookups/noneuimportcols.json",
+        "dispatches":"data/lookups/eutradecols.json",
+        "arrivals":"data/lookups/eutradecols.json"
+    }
+
+    # Table Specification Loads
+    controlfilecols = json.loads(open(table_specs["control"], "r").read())
+    eutradecols = json.loads(open(table_specs["arrivals"], "r").read())
+    noneuimportcols = json.loads(open(table_specs["imports"], "r").read())
+    noneuexportcols = json.loads(open(table_specs["exports"], "r").read())
+
+    # Trade File Abbreviation - Table Name mapping
+    trade_files = {
+        "SMKA12":"control",                               # Commodity Lookups
+        "SMKE19":"exports", "SMKI19":"imports",           # Non EU Trade
+        "SMKX46":"dispatches", "SMKM46": "arrivals"       # EU Trade
+    }
+
+    # Recoding Dicts
+    recode_inland_mot = json.loads(open("data/lookups/recode_mode_of_transport.json", "r").read())
+    recode_border_mot = {"0"+x:y for (x,y) in recode_inland_mot.items()}
+
+
+    # DOWNLOAD DATA ======================================================================
+    update_path = Path(data_dir)
+    download_individual_zipfiles(data_dir, list(trade_files.keys()),
+                                 month = data_month, year = data_year)
+
+    # LOAD TO DATABASE ===================================================================
+    # TODO Test if month has been loaded
+    # TODO Data Load
+
+    # Connect to Database
+    engine = connect_to_postgres(username = "postgres", password = "postgres",
+                                 host = "localhost", database = "trade")
+
+    #
